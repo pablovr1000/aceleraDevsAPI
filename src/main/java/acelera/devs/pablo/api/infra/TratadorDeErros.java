e40d95aed019495a9c53938c16cf9aa6a4ddc5b0
pablo <pablovr1000@hotmail.com>
2024-02-15 14:10:33 -0300

    dev: Tratando erro 400 com retorno adequado

--- src/main/java/acelera/devs/pablo/api/infra/TratadorDeErros.java
+++ src/main/java/acelera/devs/pablo/api/infra/TratadorDeErros.java
@@ -2,6 +2,8 @@ package acelera.devs.pablo.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,5 +12,16 @@ public class TratadorDeErros {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity Error404() {
         return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity Error400(MethodArgumentNotValidException e) {
+        var erro = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(erro.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
